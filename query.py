11e8f995f7a9805b69dd27a3934a765dff2fab16
Lingrui <lingruicai@gmail.com>
2018-02-28 20:16:40 -0500

    add tables

--- query.py
+++ query.py
@@ -20,6 +20,41 @@ def test_balance (table):
     text = pq(table).text()
     return "Assets" in text and "Liabilities" in text
 def parse_balance (bigtable, table):
+    '''
+    i = 0
+    bigdic = {}
+    rows = []
+    ass = {}
+    lia = {}
+    sto = {}
+    col1 = col2 = col3 = col4 = {}
+    cn1 = cn2 = cn3 = cn4 = ''
+ 
+    for tr in pq(table)('tr'):
+        td1,td2,td3,td4 = pq(tr)('td')
+        if i == 0:
+            cn1 = pq(td2).text()
+            cn2 = pq(td3).text()
+            cn3 = pq(td4).text()
+            cn4 = pq(td5).text()
+            pass
+        elif i < 7:
+            col1[pq(td1).text()] = pq(td2).text()    
+            col2[pq(td1).text()] = pq(td3).text()    
+            col3[pq(td1).text()] = pq(td4).text()    
+            col4[pq(td1).text()] = pq(td5).text()    
+            pass
+        elif i > 8:
+            ass[pq(td1).text()] = pq(td2).text()    
+            liq[pq(td1).text()] = pq(td2).text()    
+            pass
+        i += 1 
+        pass
+    bigdic["Asset"] = ass
+    bigdic["Liquidity"] = liq
+    bigdic["Leverage"] = lev
+    rows.append(bigdic)
+    '''
     bigtable['Balance Sheet(Millions)'] = pq(table).text()
     pass
 
@@ -110,7 +145,34 @@ def test_income(table):
     return "Total Revenues(Net Sales)" in text and "Selling & Admin Exps" in text 
 
 def parse_income(bigtable,table):
-    bigtable['Income Statement(Millions)'] = pq(table).text()
+    i = 0
+    bigdic = {}
+    rows = []
+    col1 = col2 = col3 = col4 = {}
+    cn1 = cn2 = cn3 = cn4 = ''
+    for tr in pq(table)('tr'):
+        td1,td2,td3,td4,td5 = pq(tr)('td')
+        if i == 0:
+            cn1 = pq(td2).text()
+            cn2 = pq(td3).text()
+            cn3 = pq(td4).text()
+            cn4 = pq(td5).text()
+            pass
+        else:
+            col1[pq(td1).text()] = pq(td2).text()    
+            col2[pq(td1).text()] = pq(td3).text()    
+            col3[pq(td1).text()] = pq(td4).text()    
+            col4[pq(td1).text()] = pq(td5).text()    
+            pass
+        i += 1 
+        pass
+    bigdic[cn1] = col1
+    bigdic[cn2] = col2
+    bigdic[cn3] = col3
+    bigdic[cn4] = col4
+    rows.append(bigdic)
+    bigtable['Income Statement(Millions)'] = rows 
+    #bigtable['Income Statement(Millions)'] = pq(table).text()
     pass
 
 def test_cash(table):
@@ -118,7 +180,33 @@ def test_cash(table):
     return "Operating Activities" in text and "Investing Activities" in text 
 
 def parse_cash(bigtable,table):
-    bigtable['Cash Flow Summary(Millions)'] = pq(table).text()
+    i = 0
+    heads = []
+    rows = []
+    #print (pq(table).text())
+    for tr in pq(table)('tr'):
+        #print(tr.text)
+        if i == 0:
+            for td in pq(tr)('td'):
+                #print (pq(td).text())
+                heads.append(pq(td).text())
+                pass
+            pass
+        else:
+            contents = []
+            h = {}
+            for td in pq(tr)('td'):
+                contents.append(pq(td).text())
+                pass
+            for head,content in zip(heads,contents):
+                #print (head,content)
+                h[head] = content
+                pass
+            rows.append(h)
+        i += 1 
+        pass
+    bigtable['Cash Flow Summary(Millions)'] = rows 
+    #bigtable['Cash Flow Summary(Millions)'] = pq(table).text()
     pass
 
 def test_annual(table):
@@ -126,7 +214,33 @@ def test_annual(table):
     return "Growth Rates" in text and "Net Income" in text 
 
 def parse_annual(bigtable,table):
-    bigtable['Annual Summary Data(Millions)'] = pq(table).text()
+    i = 0
+    heads = []
+    rows = []
+    #print (pq(table).text())
+    for tr in pq(table)('tr'):
+        #print(tr.text)
+        if i == 0:
+            for td in pq(tr)('td'):
+                #print (pq(td).text())
+                heads.append(pq(td).text())
+                pass
+            pass
+        else:
+            contents = []
+            h = {}
+            for td in pq(tr)('td'):
+                contents.append(pq(td).text())
+                pass
+            for head,content in zip(heads,contents):
+                #print (head,content)
+                h[head] = content
+                pass
+            rows.append(h)
+        i += 1 
+        pass
+    bigtable['Annual Summary Data(Millions)'] = rows
+    #bigtable['Annual Summary Data(Millions)'] = pq(table).text()
     pass
 
 def test_stock(table):
@@ -134,7 +248,33 @@ def test_stock(table):
     return "No. Owners" in text and "Shares Held (000s)" in text 
 
 def parse_stock(bigtable,table):
-    bigtable['Stock Ownership'] = pq(table).text()
+    i = 0
+    heads = []
+    rows = []
+    #print (pq(table).text())
+    for tr in pq(table)('tr'):
+        #print(tr.text)
+        if i == 0:
+            for td in pq(tr)('td'):
+                #print (pq(td).text())
+                heads.append(pq(td).text())
+                pass
+            pass
+        else:
+            contents = []
+            h = {}
+            for td in pq(tr)('td'):
+                contents.append(pq(td).text())
+                pass
+            for head,content in zip(heads,contents):
+                #print (head,content)
+                h[head] = content
+                pass
+            rows.append(h)
+        i += 1 
+        pass
+    bigtable['Stock Ownership'] = rows 
+    #bigtable['Stock Ownership'] = pq(table).text()
     pass
 
 parser = [(test_balance, parse_balance), 
@@ -189,9 +329,9 @@ def get_sec(symbol):
         pass
 
 
-    print json.dumps(bigtable)
-    pass
-    #return json.dumps(bigtable)
+    #print json.dumps(bigtable)
+    #pass
+    return json.dumps(bigtable)
 '''
 sys.exit(0)
 
@@ -240,6 +380,6 @@ for tr in data.items():
 f.close()
 '''
 if __name__ == '__main__':
-    get_sec('goog')
+    #get_sec('goog')
     pass
     
